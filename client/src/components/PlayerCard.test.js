24d34e69dae88769e759cdd8992b5703dd22bf5a
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-02-21 20:53:26 -0500

    Made small change to dark mode player card background color change. When testing app renders however, when testing playercard it fails due to map not being defined?

---
+++ client/src/components/PlayerCard.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import PlayerCard from './PlayerCard';
+
+test('Player Card renders without crashing!', () => {
+	render(<PlayerCard />);
+});
+
+// test('testing for players name', () => {
+// 	const { getByText } = render(<PlayerCard />);
+
+// 	getByText(/country/i);
+// });
